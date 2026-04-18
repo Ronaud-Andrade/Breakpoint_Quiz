4775c746cbb5d328b0a902425918feb42a58b4d2
Ronaud Andrade Lacerda Clementino <ronaudandrade108@gmail.com>
2026-04-18 16:16:17 -0300

    Commit

--- app/(tabs)/questions.tsx
+++ app/(tabs)/questions.tsx
@@ -3,14 +3,26 @@ import { LinearGradient } from 'expo-linear-gradient';
 
 export default function QuestionsScreen(){
   return(
-    <View style={styles.conteiner}>
+    <View style={{flex:1}}>
         <LinearGradient
         style={styles.conteiner}
         colors={['#00AEFF', '#00FF4D']}
         >
-            <View>
-                <Text style={{fontSize: 20, color:'white', fontWeight: 'bold'}}>Questions Screen</Text>
+            <View style={{alignItems: 'center', justifyContent: 'space-between', padding: 20}}>
+                <Text style={{fontSize: 20, color:'white', fontWeight: 'bold', marginTop: 60}}>Perguntas 1 de 1</Text> {/* Usar useState para o número de perguntas */}
+                
+                <Text style={{fontSize: 19, color:'white', fontWeight: 'bold', padding: 30, textAlign:'center'}}>
+                  Qual linguagem é usada para estilizar páginas web?
+                </Text>
+
+                {/* Criar componentes para os botões de respostas */}
+
+                
             </View>
+
+
+
+
         </LinearGradient>
     </View>
   )
@@ -21,5 +33,7 @@ const styles = StyleSheet.create({
     flex:1,
     width: '100%',
     height: '100%',
+    alignItems: 'center',
+    justifyContent: 'space-between',
   },
 });
